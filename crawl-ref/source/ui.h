fe0af876aa5280d704a5536bd89182beff1990a4
Aidan Holm <aidanholm@gmail.com>
2018-01-29 10:58:44 +0800

    Make menu columns as narrow as possible

--- crawl-ref/source/ui.h
+++ crawl-ref/source/ui.h
@@ -9,6 +9,7 @@
 #include <functional>
 
 #include "format.h"
+#include "KeymapContext.h"
 #include "tilefont.h"
 #include "tiledef-gui.h"
 #include "windowmanager.h"
@@ -510,10 +511,11 @@ protected:
 };
 #endif
 
-void ui_push_layout(shared_ptr<UI> root);
+void ui_push_layout(shared_ptr<UI> root, KeymapContext km = KMC_DEFAULT);
 void ui_pop_layout();
 void ui_pump_events();
 void ui_run_layout(shared_ptr<UI> root, const bool& done);
+int ui_getch(KeymapContext km = KMC_DEFAULT);
 
 void ui_push_scissor(i4 scissor);
 void ui_pop_scissor();
