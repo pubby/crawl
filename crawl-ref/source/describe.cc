c23fcd427ee77497751be35150b7d984ce8e292e
Aidan Holm <aidanholm@gmail.com>
2018-02-20 21:22:44 +0800

    Shrink webtiles CRT popup in some places
    This is something of a hack; ideally, the server would send a JSON
    message containing the data required for the webtiles client to render
    its own view of popups.

--- crawl-ref/source/describe.cc
+++ crawl-ref/source/describe.cc
@@ -177,7 +177,7 @@ int show_description(const describe_info &inf, const tile_def *tile)
     });
 
 #ifdef USE_TILE_WEB
-    tiles_crt_control show_as_menu(CRT_MENU);
+    tiles_crt_control show_as_menu(CRT_MENU, "crt_shrink");
 #endif
 
     ui_run_layout(move(popup), done);
@@ -2437,7 +2437,7 @@ void describe_feature_wide(const coord_def& pos)
     });
 
 #ifdef USE_TILE_WEB
-    tiles_crt_control show_as_menu(CRT_MENU);
+    tiles_crt_control show_as_menu(CRT_MENU, "crt_shrink");
 #endif
 
     ui_run_layout(move(popup), done);
@@ -2793,7 +2793,7 @@ bool describe_item(item_def &item, function<void (string&)> fixup_desc)
     });
 
 #ifdef USE_TILE_WEB
-    tiles_crt_control show_as_menu(CRT_MENU);
+    tiles_crt_control show_as_menu(CRT_MENU, "crt_shrink");
 #endif
 
     ui_run_layout(move(popup), done);
@@ -3224,7 +3224,7 @@ void describe_spell(spell_type spell, const monster_info *mon_owner,
     });
 
 #ifdef USE_TILE_WEB
-    tiles_crt_control show_as_menu(CRT_MENU);
+    tiles_crt_control show_as_menu(CRT_MENU, "crt_shrink");
 #endif
 
     ui_run_layout(move(popup), done);
@@ -4514,7 +4514,7 @@ int describe_monsters(const monster_info &mi, bool force_seen,
     });
 
 #ifdef USE_TILE_WEB
-    tiles_crt_control show_as_menu(CRT_MENU);
+    tiles_crt_control show_as_menu(CRT_MENU, "crt_shrink");
 #endif
 
     ui_run_layout(move(popup), done);
