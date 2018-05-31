7312e6b7fbfb8cec282718182bbe68f19e18f02b
pubby <pubby8@gmail.com>
2018-05-31 14:48:31 -0500

    New species: Oni
    a.k.a Japanese ogre demon things.
    
    Oni magic works differently than other species. Instead of learning spells
    through books, Oni learn spells when they level up. Additionally,
    Oni cast using spellcasting only, which replaces individual spell
    schools for them.
    
    Formula: Gain 1 random spell every 2 XLs, up to level 26.
             (A total of 13 spells per game.)
             Low level spells are gained first, then strong ones.
    
    These two changes focus the gameplay around using a small set of
    spells of differing schools. Unlike other casters, your magic toolbox
    is very limited, but unique. You might get a single conjuration, a single
    summon, a charm, a necromancy, and two hexes, and you have to figure out how
    to tactically use them.
    
    The species has fairly flat  aptitudes and a few monstrous innate mutations.
    They are hated by holy gods.
    
    Tweak Oni apts
    
    Raising weapon apts makes hybrids more viable, as in some games you'll
    get charms instead of conjurations.
    
    Dummy oni monster
    
    Recommend Gl and Tm to Oni

--- crawl-ref/source/newgame.cc
+++ crawl-ref/source/newgame.cc
@@ -196,6 +196,7 @@ static const species_type species_order[] =
     SP_VINE_STALKER,
     // celestial species
     SP_DEMIGOD,        SP_DEMONSPAWN,
+    SP_ONI,
     // undead species
     SP_MUMMY,          SP_GHOUL,
     SP_VAMPIRE,
@@ -973,6 +974,7 @@ static species_group species_groups[] =
             SP_DEEP_DWARF,
             SP_DUSK_WALKER,
             SP_GNOLL,
+            SP_ONI,
         }
     },
     {
